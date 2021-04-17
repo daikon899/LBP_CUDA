9a8d0ecd8faeb5c77380bd66134e87034dcf4317
Simone Pezzulla <simone.pezzulla@stud.unifi.it>
2021-04-17 12:21:31 +0200

    algorithm now works

--- LBPCuda/src/LBPCuda.cu
+++ LBPCuda/src/LBPCuda.cu
@@ -14,7 +14,9 @@ using namespace std;
 using namespace cv;
 using namespace std::chrono;
 
-static void CheckCudaErrorAux (const char *file, unsigned line, const char *statement, cudaError_t err)
+__constant__ int weights[8];
+
+ static void CheckCudaErrorAux (const char *file, unsigned line, const char *statement, cudaError_t err)
 {
 	if (err == cudaSuccess)
 		return;
@@ -25,7 +27,6 @@ static void CheckCudaErrorAux (const char *file, unsigned line, const char *stat
 #define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__,__LINE__, #value, value)
 
 //---------------------------------------------------------------------------------------------------------------------------------------
-
 
 __global__ void warm_up_gpu(){  // this kernel avoids cold start when evaluating duration of kmeans exec.
   unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -38,97 +39,94 @@ __global__ void warm_up_gpu(){  // this kernel avoids cold start when evaluating
 
 __global__ void lbpApply(unsigned char *imgIn_d, unsigned char *imgOut_d, int *histogram_d, int rows, int cols){
 
-	int i = blockIdx.y * blockDim.y + threadIdx.y; //rows
-	int j = blockIdx.x * blockDim.x + threadIdx.x; //cols
+	int i = blockIdx.y * blockDim.y + threadIdx.y; //row
+	int j = blockIdx.x * blockDim.x + threadIdx.x; //col
 
-	//create weights
-	__shared__ int weights[8];
-	if(i < 8 && j == 1) {
-		weights[i] = 2^i;
-	}
-	__syncthreads();
+	//+++++++++++++++++++++++++++++++++++++++++++missing histogram and tiling++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-	if (i > 0 && i < rows && j > 0 && j < cols){
-		imgOut_d[i * cols + j] = imgIn_d[(i + 1) * cols + j + 1 ];
-		/*
+	if (i < rows && j < cols){
 		int neighbors[8];
-		neighbors[0] = imgIn.at<uchar>(i - 1, j - 1);
-		neighbors[1] = imgIn.at<uchar>(i - 1, j);
-		neighbors[2] = imgIn.at<uchar>(i - 1, j + 1);
-		neighbors[3] = imgIn.at<uchar>(i, j + 1);
-		neighbors[4] = imgIn.at<uchar>(i + 1, j + 1);
-		neighbors[5] = imgIn.at<uchar>(i + 1, j);
-		neighbors[6] = imgIn.at<uchar>(i + 1, j - 1);
-		neighbors[7] = imgIn.at<uchar>(i, j - 1);
-		int oldVal = imgIn.at<uchar>(i, j);
+
+		// remember that imgOut_d[i * cols + j] -> imgIn_d[ (i + 1) * (cols + 2) + j + 1 ];
+
+		neighbors[0] = imgIn_d[(i) * (cols + 2) + j]; // (i - 1, j - 1);
+		neighbors[1] = imgIn_d[(i) * (cols + 2) + j + 1]; // (i - 1, j);
+		neighbors[2] = imgIn_d[(i) * (cols + 2) + j + 2]; // (i - 1, j + 1);
+		neighbors[3] = imgIn_d[(i + 1) * (cols + 2) + j + 2]; // (i, j + 1);
+		neighbors[4] = imgIn_d[(i + 2) * (cols + 2) + j + 2]; // (i + 1, j + 1);
+		neighbors[5] = imgIn_d[(i + 2) * (cols + 2) + j + 1]; // (i + 1, j);
+		neighbors[6] = imgIn_d[(i + 2) * (cols + 2) + j]; // (i + 1, j - 1);
+		neighbors[7] = imgIn_d[(i + 1) * (cols + 2) + j]; // (i, j - 1);
+
+		int oldVal = imgIn_d[ (i + 1) * (cols + 2) + j + 1 ]; // (i, j);
+
 		int newVal = 0;
 		for (int k = 0; k < 8; k++) {
 			if (neighbors[k] >= oldVal)
 				newVal += weights[k];
 		}
-		imgOut.at<uchar>(i - 1, j - 1) = newVal;
-		histogram[newVal]++;
-		*/
+		imgOut_d[i * cols + j] = newVal;
+
+		//histogram[newVal]++;
+
 	}
 
 }
 
 
-
-__host__ Mat localBinaryPattern(const Mat &imgIn_h) {
-
-
-    // output image
-    unsigned char *imgOut_d;
-    size_t imgOutSize = imgIn_h.step * imgIn_h.rows;
-    CUDA_CHECK_RETURN(cudaMalloc((void ** )&imgOut_d, imgOutSize));
-    Mat imgOut_h = Mat::zeros(imgIn_h.rows, imgIn_h.cols, CV_8UC1);
+// TODO insert main code in this function:
+//__host__ Mat localBinaryPattern(const Mat &imgIn_h) {}
 
 
-    //input image
-    unsigned char *imgIn_d;
-    // copyMakeBorder(imgIn_h, imgIn_h, 1, 1, 1, 1, BORDER_CONSTANT, 0); //FIXME error if uncomment this
-    size_t imgInSize = imgIn_h.step * imgIn_h.rows;
-    CUDA_CHECK_RETURN(cudaMalloc((void ** )&imgIn_d, imgInSize));
-    CUDA_CHECK_RETURN(cudaMemcpy(imgIn_d, imgIn_h.data, imgInSize, cudaMemcpyHostToDevice));
+int main(int argc, char **argv){
+	String imgName = argv[1];
+	Mat imgIn_h = cv::imread("input/" + imgName, 0);
 
-    //histogram
-    int *histogram_h, *histogram_d;
-    histogram_h = (int *) malloc(sizeof(int) * 256);
-    CUDA_CHECK_RETURN(cudaMalloc((void ** )&histogram_d, sizeof(int) * 256));
+	//imshow("Image before LBP", imgIn_h);
+	//warm_up_gpu<<<128, 128>>>();  // avoids cold start for testing purposes
+	auto start = chrono::high_resolution_clock::now();
+
+	// output image
+	unsigned char *imgOut_d;
+	size_t imgOutSize = imgIn_h.step * imgIn_h.rows;
+	CUDA_CHECK_RETURN(cudaMalloc((void ** )&imgOut_d, imgOutSize));
+	Mat imgOut_h = Mat::zeros(imgIn_h.rows, imgIn_h.cols, CV_8UC1);
+
+
+	//input image
+	unsigned char *imgIn_d;
+	copyMakeBorder(imgIn_h, imgIn_h, 1, 1, 1, 1, BORDER_CONSTANT, 0);
+	size_t imgInSize = imgIn_h.step * imgIn_h.rows;
+	CUDA_CHECK_RETURN(cudaMalloc((void ** )&imgIn_d, imgInSize));
+	CUDA_CHECK_RETURN(cudaMemcpy(imgIn_d, imgIn_h.data, imgInSize, cudaMemcpyHostToDevice));
+
+	//histogram
+	int *histogram_h, *histogram_d;
+	histogram_h = (int *) malloc(sizeof(int) * 256);
+	CUDA_CHECK_RETURN(cudaMalloc((void ** )&histogram_d, sizeof(int) * 256));
 	CUDA_CHECK_RETURN(cudaMemset(histogram_d, 0, sizeof(int) * 256 ));
 
+	//weights
+	int weights_h[8] = {1, 2, 4, 8, 16, 32, 64, 128};
+	cudaMemcpyToSymbol(weights, &weights_h, sizeof(int) * 8);
 
-	dim3 gridDim(ceil(imgIn_h.rows), ceil(imgIn_h.cols));
+
 	dim3 blockDim(16,16);
+	dim3 gridDim(ceil( (float) imgOut_h.cols / blockDim.x), ceil( (float) imgOut_h.rows / blockDim.y) );
 	lbpApply<<<gridDim, blockDim>>>(imgIn_d, imgOut_d, histogram_d, imgOut_h.rows, imgOut_h.cols);
 	cudaDeviceSynchronize();
 
 	CUDA_CHECK_RETURN(cudaMemcpy(imgOut_h.data, imgOut_d, imgOutSize, cudaMemcpyDeviceToHost));
 	CUDA_CHECK_RETURN(cudaMemcpy(histogram_h, histogram_d, sizeof(int) * 256, cudaMemcpyDeviceToHost));
 
-    //writeCsv(histogram_h);
-
-    return imgOut_h;
-}
+	//writeCsv(histogram_h);
 
 
-int main(int argc, char **argv){
-	String imgName = argv[1];
-	Mat inputImg = cv::imread("input/" + imgName, 0);
-
-	imshow("Image before LBP", inputImg);
-	//warm_up_gpu<<<128, 128>>>();  // avoids cold start for testing purposes
-	auto start = chrono::high_resolution_clock::now();
-
-    copyMakeBorder(inputImg, inputImg, 1, 1, 1, 1, BORDER_CONSTANT, 0); // TODO add this to localbinarypattern function
-
-	Mat outputImg = localBinaryPattern(inputImg);
 	auto end = chrono::high_resolution_clock::now();
 	auto ms_int = duration_cast<chrono::milliseconds>(end - start);
 
-	imshow("Image after LBP", outputImg);
+	imshow("Image after LBP", imgOut_h);
 	waitKey(0);
 
 	return ms_int.count();
