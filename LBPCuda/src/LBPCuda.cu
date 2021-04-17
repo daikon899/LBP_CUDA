7e59f71920ebeaf0f1ea6c1e51e043a356efd55f
Simone Pezzulla <simone.pezzulla@stud.unifi.it>
2021-04-17 09:20:19 +0200

    Allocating memory for histogram

--- LBPCuda/src/LBPCuda.cu
+++ LBPCuda/src/LBPCuda.cu
@@ -40,14 +40,14 @@ __global__ void lbpApply(unsigned char *imgIn_d, unsigned char *imgOut_d, int *h
 
 	int i = blockIdx.y * blockDim.y + threadIdx.y; //rows
 	int j = blockIdx.x * blockDim.x + threadIdx.x; //cols
-/*
+
 	//create weights
-	__shared__ const int weights[8];
+	__shared__ int weights[8];
 	if(i < 8 && j == 1) {
 		weights[i] = 2^i;
 	}
-	__synchthreads();
-*/
+	__syncthreads();
+
 
 	if (i > 0 && i < rows && j > 0 && j < cols){
 		imgOut_d[i * cols + j] = imgIn_d[(i + 1) * cols + j + 1 ];
@@ -61,9 +61,7 @@ __global__ void lbpApply(unsigned char *imgIn_d, unsigned char *imgOut_d, int *h
 		neighbors[5] = imgIn.at<uchar>(i + 1, j);
 		neighbors[6] = imgIn.at<uchar>(i + 1, j - 1);
 		neighbors[7] = imgIn.at<uchar>(i, j - 1);
-
 		int oldVal = imgIn.at<uchar>(i, j);
-
 		int newVal = 0;
 		for (int k = 0; k < 8; k++) {
 			if (neighbors[k] >= oldVal)
@@ -97,6 +95,7 @@ __host__ Mat localBinaryPattern(const Mat &imgIn_h) {
 
     //histogram
     int *histogram_h, *histogram_d;
+    histogram_h = (int *) malloc(sizeof(int) * 256);
     CUDA_CHECK_RETURN(cudaMalloc((void ** )&histogram_d, sizeof(int) * 256));
 	CUDA_CHECK_RETURN(cudaMemset(histogram_d, 0, sizeof(int) * 256 ));
 
@@ -107,7 +106,7 @@ __host__ Mat localBinaryPattern(const Mat &imgIn_h) {
 	cudaDeviceSynchronize();
 
 	CUDA_CHECK_RETURN(cudaMemcpy(imgOut_h.data, imgOut_d, imgOutSize, cudaMemcpyDeviceToHost));
-	//CUDA_CHECK_RETURN(cudaMemcpy(histogram_h, (void **)&histogram_d, sizeof(int) * 256, cudaMemcpyDeviceToHost));
+	CUDA_CHECK_RETURN(cudaMemcpy(histogram_h, histogram_d, sizeof(int) * 256, cudaMemcpyDeviceToHost));
 
     //writeCsv(histogram_h);
 
@@ -119,7 +118,7 @@ int main(int argc, char **argv){
 	String imgName = argv[1];
 	Mat inputImg = cv::imread("input/" + imgName, 0);
 
-	//imshow("Image before LBP", inputImg);
+	imshow("Image before LBP", inputImg);
 	//warm_up_gpu<<<128, 128>>>();  // avoids cold start for testing purposes
 	auto start = chrono::high_resolution_clock::now();
 
