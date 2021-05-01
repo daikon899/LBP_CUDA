37acdc10f34b9298a0879ab10687eb804678ed28
MarcoSolarino <marcosolarino@live.it>
2021-05-01 16:51:31 +0200

    - added a second version of the algorithm with reduced global memory accesses

--- LBPCuda/src/LBPCuda.cu
+++ LBPCuda/src/LBPCuda.cu
@@ -55,10 +55,12 @@ __global__ void lbpApplyS(unsigned char *imgIn_d, unsigned char *imgOut_d, int *
 
 	//load one part of image in shared memory
 	int beginLoad = (blockIdx.y * blockDim.y) * colsB + blockIdx.x * blockDim.x;
-	imgIn_s[tid] = imgIn_d[ beginLoad +  ((tid / TILE_WIDTH) * colsB) + (tid % TILE_WIDTH) ];
+	int imgLocation =  beginLoad + ((tid / TILE_WIDTH) * colsB) + (tid % TILE_WIDTH);
+	imgIn_s[tid] = imgIn_d[imgLocation];
 	if (tid < NUM_BORDER_PIXELS){
-		tid += BLOCK_WIDTH * BLOCK_WIDTH;
-		imgIn_s[tid] = imgIn_d[ beginLoad +  ((tid / TILE_WIDTH) * colsB) + (tid %  TILE_WIDTH) ];
+		int border = tid + (BLOCK_WIDTH * BLOCK_WIDTH);
+		imgLocation = beginLoad + ((border / TILE_WIDTH) * colsB) + (border %  TILE_WIDTH);
+		imgIn_s[border] = imgIn_d[ imgLocation ];
 	}
 	__syncthreads();
 
@@ -77,7 +79,58 @@ __global__ void lbpApplyS(unsigned char *imgIn_d, unsigned char *imgOut_d, int *
 	__syncthreads();
 
 	//commit histogram to global memory
-	atomicAdd(&histogram_d[bi * 16 + bj], histogram_s[bi * 16 + bj]);
+	atomicAdd(&histogram_d[tid], histogram_s[tid]);
+}
+
+__global__ void lbpApplyV2(unsigned char *imgIn_d, unsigned char *imgOut_d, int *histogram_d, int rows, int cols){
+	int bi = threadIdx.y;
+	int bj = threadIdx.x;
+	int i = blockIdx.y * blockDim.y + bi; //row of imgOut
+	int j = blockIdx.x * blockDim.x + bj; //col of imgOut
+	int colsB = cols + 2; //columns number considering border
+
+
+	__shared__ unsigned char imgIn_s[TILE_WIDTH * TILE_WIDTH];
+	__shared__ int histogram_s[256];
+
+	int tid = bi * BLOCK_WIDTH + bj;
+	histogram_s[tid] = 0; // NOTE: if BLOCK_WIDTH != 16 does not work!
+	imgIn_s[tid] = 0;
+	if(tid < NUM_BORDER_PIXELS) {
+		int border = tid + (BLOCK_WIDTH * BLOCK_WIDTH);
+		imgIn_s[border] = 0;
+	}
+
+	//load one part of image in shared memory
+	int beginLoad = (blockIdx.y * blockDim.y) * colsB + blockIdx.x * blockDim.x;
+	int imgLocation =  beginLoad + ((tid / TILE_WIDTH) * colsB) + (tid % TILE_WIDTH);
+	if(imgLocation < (rows + 2) * (cols + 2))
+		imgIn_s[tid] = imgIn_d[imgLocation];
+
+	if (tid < NUM_BORDER_PIXELS){
+		int border = tid + (BLOCK_WIDTH * BLOCK_WIDTH);
+		imgLocation = beginLoad + ((border / TILE_WIDTH) * colsB) + (border %  TILE_WIDTH);
+		if(imgLocation < (rows + 2) * (cols + 2))
+			imgIn_s[border] = imgIn_d[imgLocation];
+	}
+	__syncthreads();
+
+	//elaboration of neighbors
+	if (i < rows && j < cols){
+		int oldVal = imgIn_s[(bi + 1) * TILE_WIDTH + (bj + 1)];
+		int newVal = 0;
+		for (int u = 0; u < 3; u++)
+			for (int v = 0; v < 3; v++)
+				if (imgIn_s[(bi + u) * TILE_WIDTH + (bj + v)] >= oldVal)
+					newVal += weights[u][v];
+
+		imgOut_d[i * cols + j] = newVal;
+		atomicAdd(&histogram_s[newVal], 1);
+	}
+	__syncthreads();
+
+	//commit histogram to global memory
+	atomicAdd(&histogram_d[tid], histogram_s[tid]);
 }
 
 
@@ -127,7 +180,8 @@ __host__ Mat localBinaryPattern(Mat &imgIn_h) {
 
 	//input image
 	unsigned char *imgIn_d;
-	copyMakeBorder(imgIn_h, imgIn_h, 1, BLOCK_WIDTH - 1, 1, BLOCK_WIDTH - 1, BORDER_CONSTANT, 0);
+	//copyMakeBorder(imgIn_h, imgIn_h, 1, BLOCK_WIDTH - 1, 1, BLOCK_WIDTH - 1, BORDER_CONSTANT, 0);
+	copyMakeBorder(imgIn_h, imgIn_h, 1, 1, 1, 1, BORDER_CONSTANT, 0); // borders for lbpApplyV2
 	size_t imgInSize = imgIn_h.step * imgIn_h.rows;
 	CUDA_CHECK_RETURN(cudaMalloc((void ** )&imgIn_d, imgInSize));
 	CUDA_CHECK_RETURN(cudaMemcpy(imgIn_d, imgIn_h.data, imgInSize, cudaMemcpyHostToDevice));
@@ -145,7 +199,8 @@ __host__ Mat localBinaryPattern(Mat &imgIn_h) {
 	dim3 blockDim(BLOCK_WIDTH, BLOCK_WIDTH);
 	dim3 gridDim(ceil( (float) imgOut_h.cols / blockDim.x), ceil( (float) imgOut_h.rows / blockDim.y) );
 	//lbpApply<<<gridDim, blockDim>>>(imgIn_d, imgOut_d, histogram_d, imgOut_h.rows, imgOut_h.cols);
-	lbpApplyS<<<gridDim, blockDim>>>(imgIn_d, imgOut_d, histogram_d, imgOut_h.rows, imgOut_h.cols);
+	//lbpApplyS<<<gridDim, blockDim>>>(imgIn_d, imgOut_d, histogram_d, imgOut_h.rows, imgOut_h.cols);
+	lbpApplyV2<<<gridDim, blockDim>>>(imgIn_d, imgOut_d, histogram_d, imgOut_h.rows, imgOut_h.cols);
 	cudaDeviceSynchronize();
 
 	CUDA_CHECK_RETURN(cudaMemcpy(imgOut_h.data, imgOut_d, imgOutSize, cudaMemcpyDeviceToHost));
@@ -165,7 +220,7 @@ __host__ Mat localBinaryPattern(Mat &imgIn_h) {
 
 int main(int argc, char **argv){
 	//String imgName = argv[1];
-	String imgName = "images.jpg";
+	String imgName = "images2.jpg";
 	Mat imgIn_h = cv::imread("input/" + imgName, 0);
 
 
@@ -178,8 +233,8 @@ int main(int argc, char **argv){
 	auto end = chrono::high_resolution_clock::now();
 	auto ms_int = duration_cast<chrono::milliseconds>(end - start);
 
-	imshow("Image after LBP", imgOut_h);
-	waitKey(0);
+	//imshow("Image after LBP", imgOut_h);
+	//waitKey(0);
 
 	int time = ms_int.count();
 
